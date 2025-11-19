ef7135b0ff5a4f071e5d5b5450ee1a9bee5c9e89
Happy <mati.chino20@gmail.com>
2025-11-19 19:26:24 -0300

    a

---
+++ Model/Objetos_M.py
@@ -0,0 +1,15 @@
+
+class InsumoModel:
+    def __init__(self, id, nombre_usuario, clave,apellido,fecha_nacimeinto,telefono,email,tipo):
+        self.id = id
+        self.nombre_usuario = nombre_usuario
+        self.clave = clave
+        self.apellido = apellido
+        self.fecha_nacimeinto = fecha_nacimeinto
+        self.telefono = telefono
+        self.email = email
+        self.tipo = tipo
+    
+
+
+
