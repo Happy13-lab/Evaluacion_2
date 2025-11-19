11ad2a1d406bc3cb49c011abc26c751b57d6f80f
Happy <Mati.chino20@gmail.com>
2025-11-19 19:39:50 -0300

    Avance

--- Model/Objetos_M.py
+++ Model/Objetos_M.py
@@ -1,14 +1,16 @@
 
 class InsumoModel:
-    def __init__(self, id, nombre_usuario, clave,apellido,fecha_nacimeinto,telefono,email,tipo):
+    def __init__(self, id: int, nombre_usuario: str, clave: int ,apellido: str ,fecha_nacimiento: str,telefono: int,email: str,tipo: str):
         self.id = id
         self.nombre_usuario = nombre_usuario
         self.clave = clave
         self.apellido = apellido
-        self.fecha_nacimeinto = fecha_nacimeinto
+        self.fecha_nacimiento = fecha_nacimiento
         self.telefono = telefono
         self.email = email
         self.tipo = tipo
+
+    def Crear_insumo(self,id, nombre_usuario, clave, apellido, fecha_nacimiento, telefono, email, tipo) -> bool:
     
 
 
