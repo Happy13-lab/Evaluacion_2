c5587668abff9248a5edb5a17d448e976adc1c63
Happy <mati.chino20@gmail.com>
2025-12-10 19:12:59 -0300

    avance

--- Main.py
+++ Main.py
@@ -28,18 +28,47 @@ def main():
     
 
     print("BIENVENIDO A MEDIPLUS")
-    print("Registro de usuario\n")
-    id = int(input("Ingrese ID: "))
-    nombre_usuario = input("Ingrese nombre de usuario: ")
-    clave = input("Ingrese clave: ")
-    nombre = input("Ingrese nombre: ")
-    apellido = input("Ingrese apellido: ")
-    fecha_str = date(input("Ingrese fecha de nacimiento (YYYY-MM-DD): "))
-    ## CONVERTIMOS LA FECHA DE STRING A DATE
-    fecha_nacimiento = datetime.strptime (fecha_str, "%y-%m-%d").date()
-    telefono = int(input("Ingrese teléfono: "))
-    email = input("Ingrese email: ")
-    tipo = input("Ingrese tipo de usuario: ")
+    #Implementado json
+    def importar_usuarios_desde_json():
+    # Instancia del modelo y controlador
+    modelo = UsuarioModel()
+    controller = UsuarioController(modelo)
+
+    # Leer el archivo JSON
+    with open("users.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    for u in data:
+        # Generar contraseña temporal y hashearla
+        clave_hash = bcrypt.hashpw("claveTemporal123".encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+        # Registrar usuario usando tu controlador
+        controller.registrar_usuario(
+            id=u["id"],
+            nombre_usuario=u["username"],
+            clave=clave_hash,
+            nombre=u["name"],
+            apellido="",  # tu JSON no trae apellido separado
+            fecha_nacimiento=None,  # no está en el JSON
+            telefono=u["phone"],
+            email=u["email"],
+            tipo="usuario"
+        )
+
+
+    # print("Registro de usuario\n")
+    # id = int(input("Ingrese ID: "))
+    # nombre_usuario = input("Ingrese nombre de usuario: ")
+    # clave = input("Ingrese clave: ")
+    # nombre = input("Ingrese nombre: ")
+    # apellido = input("Ingrese apellido: ")
+    # fecha_str = date(input("Ingrese fecha de nacimiento (YYYY-MM-DD): "))
+
+    # ## CONVERTIMOS LA FECHA DE STRING A DATE
+    # fecha_nacimiento = datetime.strptime (fecha_str, "%y-%m-%d").date()
+    # telefono = int(input("Ingrese teléfono: "))
+    # email = input("Ingrese email: ")
+    # tipo = input("Ingrese tipo de usuario: ")
 
     usuario_controller.registrar_usuario(id, nombre_usuario, clave, nombre, apellido,
                                          fecha_nacimiento, telefono, email, tipo)
